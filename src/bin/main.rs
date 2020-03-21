07c16b99c8087cde4721792e29b6bbaec77f8eb1
Fisher Darling <fdarling@mines.edu>
2020-03-21 16:04:20 -0600

    Basic cli parsing

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,28 @@
 #![allow(non_snake_case)]
 
+use structopt::StructOpt;
 
+use std::path::PathBuf;
+
+/// LUTHER Reads a scanner definition file, a source file, and outputs matched
+/// tokens from DFA files described in the definition input.
+#[derive(Debug, Default, StructOpt)]
+#[structopt(name = "LUTHER")]
+pub struct Args {
+    /// Path to the scanning definition file
+    #[structopt(parse(from_os_str))]
+    definition: PathBuf,
+
+    /// Path to a file of source to be tokenized by LUTHER
+    #[structopt(parse(from_os_str))]
+    source: PathBuf,
+
+    /// Path to an output file for storing the tokenized source. Stdout if not present.
+    #[structopt(parse(from_os_str))]
+    output: Option<PathBuf>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::from_args();
+    println!("{:?}", args);
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,2 +1,3 @@
 /// Main struct for a scan definition file.
 pub struct Scanner;
+
